6d64545343d2ca932d41022d46f0354a134be6ff
Adeoye Tunmise Rebecca <adeoyetunmise53@gmail.com>
2023-08-17 10:10:37 -0700

    light

---
+++ bulb.js
@@ -0,0 +1,8 @@
+function turnOnLight() {
+    display.innerHTML = '<img src="../Bulb/images/snipe-removebg-preview.png" alt="" srcset="">'
+    
+}
+function turnOffLight() {
+    display.innerHTML = '<img src="../Bulb/images/snipe_1-removebg-preview.png" alt="" srcset="">'
+    
+}
